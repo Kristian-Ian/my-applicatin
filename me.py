88c8ad8c01a588d0b921847384c85911ef0ab344
Kristian-Ian <97434619+Kristian-Ian@users.noreply.github.com>
2022-01-25 10:55:05 +0300

    Add files via upload

---
+++ me.py
@@ -0,0 +1,34 @@
+tea= input("how many packets")
+milk= input("how many packets")
+bread= input("how many loaves")
+
+amount= input("amount")
+
+tea== 10
+milk== 50
+bread== 55
+
+x = "balance"
+
+if tea==10:
+    tea== tea*10
+    x= amount - tea
+else:
+    x==0
+
+if milk== 50:
+    milk== milk*50
+    x= amount-milk
+else:
+    x==0
+
+if bread==55:
+    bread== bread*55
+    x= amount- bread
+else:
+    x==0
+
+print(x)
+
+
+
